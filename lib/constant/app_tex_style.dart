0f85c51aa0a941a4cecb0034eb4aa01ab037507d
Korya25 <enthrj3@gmail.com>
2024-12-17 19:10:07 +0200

    add auth error handler file

---
+++ lib/constant/app_tex_style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static TextStyle appBar = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+  );
+
+  static TextStyle labelTextFormField = TextStyle(
+    color: Colors.white,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle hintTextFormField = TextStyle(
+    color: const Color.fromARGB(162, 211, 210, 210),
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle customDialogcontent = TextStyle(
+    color: Colors.white,
+    fontSize: 17,
+  );
+}
