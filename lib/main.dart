7e1762b1b4bbeea18173984ab8758eee45a97297
Korya25 <enthrj3@gmail.com>
2024-12-17 19:14:11 +0200

    add auth validator to validate custom text form fields and custom snak bar and custom dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat/firebase_options.dart';
+import 'package:chat/core/services/firebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
