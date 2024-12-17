2b90191bebcfc3b62630115327454a586b4dd031
Korya25 <enthrj3@gmail.com>
2024-12-17 19:25:41 +0200

    add forget pass form and screen

--- lib/features/auth/screens/forget_pass/forget_pass_form.dart
+++ lib/features/auth/screens/forget_pass/forget_pass_form.dart
@@ -1,1 +1,146 @@
+import 'package:chat/constant/app_tex_style.dart';
+import 'package:chat/core/utils/auth_validator.dart';
+import 'package:chat/core/utils/custom_dialog_and_snakbar.dart';
+import 'package:chat/features/auth/cubits/auth_cubits.dart';
+import 'package:chat/features/auth/cubits/auth_states.dart';
+import 'package:chat/features/auth/widgets/action_auth_button.dart';
+import 'package:chat/features/auth/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class ForgetPassForm extends StatefulWidget {
+  const ForgetPassForm({super.key});
+
+  @override
+  State<ForgetPassForm> createState() => _ForgetPassFormState();
+}
+
+class _ForgetPassFormState extends State<ForgetPassForm> {
+  bool isValid = false;
+  bool isEmailValid = false;
+
+  // Controller
+  final TextEditingController _emailController = TextEditingController();
+
+  /// Form Key
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  void validateForm() {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        isValid = true;
+      });
+    } else {
+      setState(() {
+        isValid = false;
+      });
+    }
+  }
+
+  void validateEmail() {
+    final email = _emailController.text;
+    setState(() {
+      isEmailValid = AuthValidators.validateEmail(email) == null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        // Listen for state changes and show appropriate feedback
+        if (state is AuthPasswordResetSuccess) {
+          // Show success message
+          Navigator.pushReplacementNamed(context, '/login');
+          CustomDialogHandler.showCustomDialog(context,
+              'we have sent you an email with instructions to recover your password');
+        }
+      },
+      child: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          final authCubit = context.watch<AuthCubit>();
+          final isLoading = authCubit is AuthLoading;
+
+          return Padding(
+            padding: const EdgeInsets.all(15),
+            child: Form(
+              key: formKey,
+              child: Column(
+                spacing: 10,
+                children: [
+                  // Email
+                  CustomTextFormField(
+                    labelText: 'Email',
+                    hintText: 'example@gmail.com',
+                    textColor: Colors.white,
+                    textEditingController: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    hintStyle: AppTextStyle.hintTextFormField,
+                    labelStyle: AppTextStyle.labelTextFormField,
+                    validator: AuthValidators.validateEmail,
+                    onSaved: (value) {
+                      _emailController.text = value ?? '';
+                    },
+                    onChanged: (_) {
+                      validateForm();
+                      validateEmail();
+                    },
+                  ),
+
+                  BlocBuilder<AuthCubit, AuthState>(
+                    builder: (context, state) {
+                      if (state is AuthFailureResetPass) {
+                        return Text(
+                          state.errorMessage,
+                          textAlign: TextAlign.start,
+                          style: TextStyle(color: Colors.red, fontSize: 15),
+                        );
+                      } else {
+                        return Container();
+                      }
+                    },
+                  ),
+
+                  // Hint
+                  Text(
+                    'Enter your email above and if an account exists we will send you an email with a link to recover your password.',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: const Color.fromARGB(162, 211, 210, 210),
+                    ),
+                  ),
+
+                  // Password Recovery Button
+                  CustomActionAuthButton(
+                    onTap: isValid && !isLoading && isEmailValid
+                        ? () {
+                            authCubit.resetPassword(
+                              email: _emailController.text,
+                              context: context,
+                            );
+                          }
+                        : null,
+                    title: isLoading
+                        ? CircularProgressIndicator()
+                        : Text(
+                            'Send Password Recovery',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                    backgroundColor: isValid && isEmailValid && !isLoading
+                        ? Colors.blue
+                        : Colors.grey,
+                    isEnabled: isValid && isEmailValid && !isLoading,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/features/auth/screens/forget_pass/forget_pass_screen.dart
+++ lib/features/auth/screens/forget_pass/forget_pass_screen.dart
@@ -1,1 +1,34 @@
+import 'package:chat/constant/app_tex_style.dart';
+import 'package:chat/core/services/auth/auth_google_services.dart';
+import 'package:chat/core/services/auth/auth_services.dart';
+import 'package:chat/features/auth/cubits/auth_cubits.dart';
+import 'package:chat/features/auth/screens/forget_pass/forget_pass_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class ForgetPasswordScreen extends StatelessWidget {
+  const ForgetPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<AuthCubit>(
+      create: (context) => AuthCubit(
+          authServices: AuthServices(),
+          googleServices:
+              AuthGoogleServices()), // تأكد من أنك تقوم بإنشاء الـ Cubit هنا
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 45, 45, 49),
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.white),
+          title: Text(
+            'Forget Password',
+            style: AppTextStyle.appBar,
+          ),
+        ),
+        body: const ForgetPassForm(),
+      ),
+    );
+  }
+}
